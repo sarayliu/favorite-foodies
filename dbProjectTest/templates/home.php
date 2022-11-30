e5056676697425cb5ffceecafa32dc9df8840485
sarayliu <sarayaoliu@gmail.com>
2022-11-29 23:10:09 -0500

    Change Enter a Review to Enter Review in nav bar

--- dbProjectTest/templates/home.php
+++ dbProjectTest/templates/home.php
@@ -73,7 +73,7 @@
               </li>
               <li class="nav-item">
                     <!-- <a class="nav-link text-dark" href="seeReviews.php"> See Reviews </a> -->
-                    <a class="nav-link btn btn-default text-light" href="?command=enterReview">Enter a Review</a>
+                    <a class="nav-link btn btn-default text-light" href="?command=enterReview">Enter Review</a>
                 </li>
               <li class="nav-item">
                   <!-- <a class="nav-link text-dark" href="editInfo.html"> Edit Information </a> -->
