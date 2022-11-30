d9981d4939e78eeb7b4ae45c0eae95c1fc049252
ssi3kaGit <ssi3ka@virginia.edu>
2022-11-29 23:30:17 -0500

    all events table styling

--- dbProjectTest/templates/allEvents.php
+++ dbProjectTest/templates/allEvents.php
@@ -15,9 +15,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
-    <link href="styles/main.css" rel="stylesheet">  
+    <!-- <link href="styles/main.css" rel="stylesheet">   -->
     
-    <link rel="stylesheet" href="styles/tables.css">
+    <!-- <link rel="stylesheet" href="styles/tables.css"> -->
 </head>
 
 <body>
